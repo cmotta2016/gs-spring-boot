89321b01ea76c2348b2e720349058a4c6474f0b1
cmotta2016 <motta.carlos08@gmail.com>
2019-10-02 15:42:02 -0300

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -27,7 +27,7 @@ public class HelloController {
         logger.info("Greetings from Spring Boot!");  
         fh.close();
     	
-        return "Greetings from Spring Boot v2!";
+        return "Greetings from Spring Boot!";
     }
 
 }
